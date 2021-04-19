3ab8a3c9944f7f3c4c38b6ac702da910f123b7bf
Animesh Shrivatri <animesh.shrivatri2002@gmail.com>
2021-04-19 13:28:08 +0530

    sidebar update

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,6 +35,10 @@ const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content : center;
+    position: relative;
+    z-index: 10;
+    box-shadow: 0 1px 0 0 rgb(255 255 255 /10%);
+
 `
 
 const Main = styled.div`
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,50 @@
 import React from 'react'
+import styled from 'styled-components';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 
 function Sidebar() {
     return (
-        <div >
-            Sidebar
-        </div>
+        <Container>
+            <WorkspaceContainer>
+                <Name>
+                    Animesh
+                </Name>
+                <NewMessage>
+                    <AddCircleOutlineIcon/>
+                </NewMessage>
+            </WorkspaceContainer>
+        </Container>
     )
 }
 
 export default Sidebar
+
+const Container = styled.div`
+    background: #3F0E40;
+`
+
+const WorkspaceContainer = styled.div`
+    color: white;
+    height: 64px;
+    display: flex;
+    align-items: center;
+    padding-left: 19px;
+    justify-content: space-between;
+    border-bottom: 1px solid #532753;
+`
+const Name = styled.div`
+
+`
+const NewMessage = styled.div`
+    width: 36px;
+    height: 36px;
+    background: white;
+    color: #3F0E40;
+    fill: #3F0E40;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    margin-right: 20px;
+    cursor: pointer;
+`
