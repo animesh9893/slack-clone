7e66ea87482f2224db69defb3453394b6305e6e2
Animesh Shrivatri <animesh.shrivatri2002@gmail.com>
2021-04-19 14:14:32 +0530

    first day done

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import {sidebarItemsData} from '../data/SidebarData'
+import AddIcon from '@material-ui/icons/Add';
 
 function Sidebar() {
     return (
@@ -13,6 +15,33 @@ function Sidebar() {
                     <AddCircleOutlineIcon/>
                 </NewMessage>
             </WorkspaceContainer>
+            <MainChannels>
+                {
+                    sidebarItemsData.map(item=>(
+                        <MainChannelItem>
+                            {item.icon}
+                            {item.text}
+                        </MainChannelItem>
+                    ))
+                }
+                
+            </MainChannels>
+            <ChannelsContainer>
+                <NewChannelContainer>
+                    <div>
+                        Channels
+                    </div>
+                    <AddIcon/>
+                </NewChannelContainer>
+                <ChannelsList>
+                    <Channel>
+                        #Channel 1
+                    </Channel>
+                    <Channel>
+                        #Channel 2
+                    </Channel>
+                </ChannelsList>
+            </ChannelsContainer>
         </Container>
     )
 }
@@ -47,4 +76,54 @@ const NewMessage = styled.div`
     border-radius: 50%;
     margin-right: 20px;
     cursor: pointer;
+`
+
+const MainChannels = styled.div`
+    padding-top: 20px;
+`
+const MainChannelItem = styled.div`
+    color : rgb(188,171,188);
+    display: grid;
+    grid-template-columns : 15% auto;
+    height: 28px;
+    align-items : center;
+    padding-left: 19px;
+    cursor: pointer;
+    :hover{
+        background: #350D36;
+    }
+
+`
+
+const ChannelsContainer = styled.div`
+    color : rgb(188,171,188);
+    margin-top: 10px;
+    
+`
+
+
+const NewChannelContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 28px;
+    padding-left: 19px;
+    padding-right: 12px;
+    
+`
+
+
+const ChannelsList = styled.div`
+    
+`
+
+const Channel = styled.div`
+    height: 28px;
+    display: flex;
+    align-items: center;
+    padding-left: 19px;
+    cursor: pointer;
+    :hover{
+        background: #350D36;
+    }
 `
