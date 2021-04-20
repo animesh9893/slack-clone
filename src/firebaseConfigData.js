396f0f7006826ccade88a39fd59ccd329c9ad684
Animesh Shrivatri <animesh.shrivatri2002@gmail.com>
2021-04-20 18:04:25 +0530

    gitignore update

---
+++ src/firebaseConfigData.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCIS8t4823BcDdB-ycYwHc99wm3yKh-oQg",
+    authDomain: "slack-clone-43316.firebaseapp.com",
+    projectId: "slack-clone-43316",
+    storageBucket: "slack-clone-43316.appspot.com",
+    messagingSenderId: "898560867419",
+    appId: "1:898560867419:web:e4aa46c0a5f957970c937e",
+    measurementId: "G-S6WM1Z4BHQ"
+  };
+
+
+export default firebaseConfig
